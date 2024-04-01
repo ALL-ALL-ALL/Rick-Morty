5ab9acf093e4bc3bd8fa59b29961dc0fcc5aa0a3
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-02 01:46:49 +0200

    en cours.......🇫🇷🛹😀🫶🌈🌞😁

--- Rick&Morty/tableau.swift
+++ Rick&Morty/tableau.swift
@@ -16,3 +16,29 @@ struct personnage{
     var type : String
 }
 
+
+var 1 = personnage(image: "", nom: "Sanchez", prenom: "Rick", type: "Hummain"),
+var 2 = personnage(image: "", nom: "Smith", prenom: "Morty", type: "Hummain"),
+var 3 = personnage(image: "", nom: "Smith", prenom: "Summer", type: "Hummain"),
+var 4 = personnage(image: "", nom: "Smith", prenom: "Beth", type: "Hummain"),
+var 5 = personnage(image: "", nom: "Smith", prenom: "Smith", type: "Hummain"),
+var 6 = personnage(image: "", nom: "ClusterPrincess", prenom: "Abadango", type: "Alien"),
+var 7 = personnage(image: "", nom: "Lincler", prenom: "Abradolf", type: "Hummain"),
+var 8 = personnage(image: "", nom: "Rick", prenom: "Adjudicator", type: "Hummain"),
+var 9 = personnage(image: "", nom: "Director", prenom: "Agency", type: "Hummain"),
+var 10 = personnage(image: "", nom: "Rails", prenom: "Alan", type: "Hummain"),
+var 11  = personnage(image: "", nom: "Einstein ", prenom: "Albert", type: "Hummain"),
+var 12 = personnage(image: "", nom: "", prenom: "Alexander", type: "Hummain"),
+var 13 = personnage(image: "", nom: "Googah ", prenom: "Alien", type: "Alien")
+
+ 
+
+
+
+
+
+
+
+
+
+
