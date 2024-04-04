93b05f7755353e12bbe0526bb7cbf0bac95d75c3
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-04-04 02:34:52 +0200

    en cours ........

--- Rick&Morty/ContentView.swift
+++ Rick&Morty/ContentView.swift
@@ -7,54 +7,63 @@
 
 import SwiftUI
 
+
+
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading){
-            ForEach(personnages){personnage in
-                HStack{
-                    Image(personnage.image)
-                        .resizable()
-                        .frame(width: 100, height: 100)
-                        .clipShape(Circle())
-                        .overlay(Circle().stroke(Color.green, lineWidth: 2))
-                        .scaledToFit()
+        NavigationView{
+            VStack(alignment: .leading){
+                ForEach(lespersonnages){personnage in
+                    NavigationLink(destination: detailView(personnage:personnage)){
+                        HStack{
+                            Image(personnage.image)
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                                .clipShape(Circle())
+                                .overlay(Circle().stroke(Color.green, lineWidth: 2))
+                                .scaledToFit()
+                                
+                                Text(personnage.prenom)
+                                .foregroundColor(.green)
+                                .bold()
+                            
+                            
+                                Text(personnage.nom)
+                                .foregroundColor(.green)
+                                .bold()
+                     
+                        } // fin Hstack
                         
-                        Text(personnage.prenom)
-                        .foregroundColor(.green)
-                        .bold()
-                        Text(personnage.nom)
-                        .foregroundColor(.green)
-                        .bold()
+                        
+                        
+                        VStack{
+                            Text(personnage.type)
+                                .bold()
+                                .padding(.leading, 110)
+                                .padding(.top, -40)
 
+                        } // fin vstack
+                        
+                        
+                        
+                        
+                        
+                    }
                     
-                        
-                        
-                   
-                   
-                } // fin vstack
+                } // fin for each
+            } // fin vstack
+
+        } // fin navigation view
                 
                 
                 
                 
                 
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-            } // fin for each
             
             
-        } // fin vstack
+            
+            
+        
         
                 
     
--- Rick&Morty/tableau.swift
+++ Rick&Morty/tableau.swift
@@ -17,36 +17,22 @@ struct personnage: Identifiable{
 }
 
 
-var personnages = [
+var lespersonnages = [
     
-    personnage(image: "ricksanchez", nom: "Sanchez", prenom: "Rick", type: "Human"),
-    personnage(image: "MortySmith", nom: "Smith", prenom: "Morty", type: "Human"),
-    personnage(image: "SummerSmith", nom: "Smith", prenom: "Summer", type: "Human"),
-    personnage(image: "Beth", nom: "Smith", prenom: "Beth", type: "Human"),
-    personnage(image: "jerrySmith", nom: "Smith", prenom: "Jerry", type: "Human"),
-    personnage(image: "AbadangoCluster", nom: "Cluster Princess", prenom: "Abadango", type: "Alien"),
-    personnage(image: "AbradolfLincler", nom: "Lincler", prenom: "Abradolf", type: "Human"),
-    personnage(image: "AdjudicatorRick", nom: "Rick", prenom: "Adjudicator", type: "Human"),
-    personnage(image: "Agency Director", nom: "Director", prenom: "Agency", type: "Human"),
-    personnage(image: "Alan", nom: "Rails", prenom: "Alan", type: "Human"),
-    personnage(image: "AlbertEinstein", nom: "Einstein ", prenom: "Albert", type: "Human"),
-    personnage(image: "alexander", nom: "", prenom: "Alexander", type: "Human"),
-    personnage(image: "AlienGoogah", nom: "Googah ", prenom: "Alien", type: "Alien")
+    Personnage(image: "ricksanchez", nom: "Sanchez", prenom: "Rick", type: "Human"),
+    Personnage(image: "MortySmith", nom: "Smith", prenom: "Morty", type: "Human"),
+    Personnage(image: "SummerSmith", nom: "Smith", prenom: "Summer", type: "Human"),
+    Personnage(image: "Beth", nom: "Smith", prenom: "Beth", type: "Human"),
+    Personnage(image: "jerrySmith", nom: "Smith", prenom: "Jerry", type: "Human"),
+    Personnage(image: "AbadangoCluster", nom: "Cluster Princess", prenom: "Abadango", type: "Alien"),
+    Personnage(image: "AbradolfLincler", nom: "Lincler", prenom: "Abradolf", type: "Human"),
+    Personnage(image: "AdjudicatorRick", nom: "Rick", prenom: "Adjudicator", type: "Human"),
+    Personnage(image: "Agency Director", nom: "Director", prenom: "Agency", type: "Human"),
+    Personnage(image: "Alan", nom: "Rails", prenom: "Alan", type: "Human"),
+    Personnage(image: "AlbertEinstein", nom: "Einstein ", prenom: "Albert", type: "Human"),
+    Personnage(image: "alexander", nom: "", prenom: "Alexander", type: "Human"),
+    Personnage(image: "AlienGoogah", nom: "Googah ", prenom: "Alien", type: "Alien")
 
 
 
 ]
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
